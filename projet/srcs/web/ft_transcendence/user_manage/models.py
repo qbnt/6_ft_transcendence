2bf32618c9e983189ce221b36e741d96fe055c36
qtn__bnt <quentin.banet@hotmail.fr>
2024-07-26 16:35:45 +0200

    Mise en bind mount du dossier web pour le docker compose. Ajout du User au projet web

--- projet/srcs/web/ft_transcendence/user_manage/models.py
+++ projet/srcs/web/ft_transcendence/user_manage/models.py
@@ -1,1 +1,18 @@
-from django.db import models
+from django.db						import models
+from django.contrib.auth.hashers	import make_password, check_password
+
+class User(models.Model):
+	username	= models.CharField(max_length=150, unique=True)
+	password	= models.CharField(max_length=128)
+	email		= models.EmailField(unique=True)
+	date_joined	= models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.username
+
+	def set_password(self, raw_pw):
+		self.password = make_password(raw_pw)
+		self.save()
+
+	def check_password(self, raw_pw):
+		return check_password(raw_pw, self.password)
