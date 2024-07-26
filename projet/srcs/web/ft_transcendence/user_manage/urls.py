4c9a7580f0ed58d48335d2470849c476f35b2128
qtn__bnt <quentin.banet@hotmail.fr>
2024-07-26 21:04:28 +0200

    Ajout d'un template de base qui sera partagé par toutes les apps

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path
-from . import views
+from django.urls	import path
+from .				import views
 
 urlpatterns = [
-    path('some-path/', views.some_view, name='some_name'),
-    # Ajoute d'autres patterns d'URL ici
+    path('', views.usr_list, name='usr_list'),
+    path('usr_view/', views.usr_view, name='usr_view'),
+    path('usr_create/', views.usr_create, name='usr_create'),
+    path('usr_edit/', views.usr_edit, name='usr_edit'),
+    path('usr_suppr/', views.usr_suppr, name='usr_suppr'),
+    path('usr_connect/', views.usr_connect, name='usr_connect'),
 ]
--- projet/srcs/web/ft_transcendence/user_manage/views.py
+++ projet/srcs/web/ft_transcendence/user_manage/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts	import render
+from django.http		import HttpResponse
+from .models			import User
 
-def some_view(request):
-    return HttpResponse("Hello, this is some view!")
+
+def usr_list(request):
+	return render(request, 'user_manage/usr_view.html')
+
+def usr_view(request):
+	pass
+
+def usr_create(request):
+	pass
+
+def usr_edit(request):
+	pass
+
+def usr_suppr(request):
+	pass
+
+def usr_connect(request):
+	pass
