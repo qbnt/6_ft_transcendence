dfc461ca053254a476f2f42a623cf9b8f03b908b
qbnt <quentin.banet@hotmail.fr>
2024-07-27 18:55:51 +0200

    Ajout de l'affichage de la liste et du detail des utilisateur et de la création de l'utilisateur.

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -2,10 +2,10 @@ from django.urls	import path
 from .				import views
 
 urlpatterns = [
-    path('', views.usr_list, name='usr_list'),
-    path('usr_view/', views.usr_view, name='usr_view'),
-    path('usr_create/', views.usr_create, name='usr_create'),
-    path('usr_edit/', views.usr_edit, name='usr_edit'),
-    path('usr_suppr/', views.usr_suppr, name='usr_suppr'),
-    path('usr_connect/', views.usr_connect, name='usr_connect'),
+    path('', views.user_list, name='user_list'),
+    path('<int:user_id>/', views.user_detail, name='user_detail'),
+    path('add/', views.user_add, name='user_add'),
+    # path('usr_edit/', views.usr_edit, name='usr_edit'),
+    # path('usr_suppr/', views.usr_suppr, name='usr_suppr'),
+    # path('usr_connect/', views.usr_connect, name='usr_connect'),
 ]
--- projet/srcs/web/ft_transcendence/user_manage/views.py
+++ projet/srcs/web/ft_transcendence/user_manage/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts	import render
-from django.http		import HttpResponse
+from django.shortcuts	import render, get_object_or_404, redirect
+from django.contrib 	import messages
 from .models			import User
+from .forms				import UserForm
 
 
-def usr_list(request):
-	return render(request, 'user_manage/usr_view.html')
+def user_list(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'user_manage/usr_list.html', context)
 
-def usr_view(request):
-	pass
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    context = {'user': user}
+    return render(request, 'user_manage/usr_detail.html', context)
 
-def usr_create(request):
-	pass
+def user_add(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Utilisateur ajouté avec succès!')
+            return redirect('user_list')
+    else:
+        form = UserForm()
+    return render(request, 'user_manage/usr_add.html', {'form': form})
 
 def usr_edit(request):
 	pass
