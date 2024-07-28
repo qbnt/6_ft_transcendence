2ed9844f820f21d8bec5a934664b0beca8c7a6ad
qbnt <quentin.banet@hotmail.fr>
2024-07-28 11:15:53 +0200

    Ajout des actions Edit et Delete

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.user_list, name='user_list'),
     path('<int:user_id>/', views.user_detail, name='user_detail'),
     path('add/', views.user_add, name='user_add'),
-    # path('usr_edit/', views.usr_edit, name='usr_edit'),
-    # path('usr_suppr/', views.usr_suppr, name='usr_suppr'),
+    path('edit/<int:user_id>', views.user_edit, name='user_edit'),
+    path('delete/<int:user_id>', views.user_delete, name='user_delete'),
     # path('usr_connect/', views.usr_connect, name='usr_connect'),
 ]
--- projet/srcs/web/ft_transcendence/user_manage/views.py
+++ projet/srcs/web/ft_transcendence/user_manage/views.py
@@ -9,11 +9,6 @@ def user_list(request):
     context = {'users': users}
     return render(request, 'user_manage/usr_list.html', context)
 
-def user_detail(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    context = {'user': user}
-    return render(request, 'user_manage/usr_detail.html', context)
-
 def user_add(request):
     if request.method == 'POST':
         form = UserForm(request.POST)
@@ -27,11 +22,33 @@ def user_add(request):
         form = UserForm()
     return render(request, 'user_manage/usr_add.html', {'form': form})
 
-def usr_edit(request):
-	pass
+def user_detail(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    context = {'user': user}
+    return render(request, 'user_manage/usr_detail.html', context)
 
-def usr_suppr(request):
-	pass
+def user_edit(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            user = form.save(commit=False)
+            if form.cleaned_data['password']:
+                user.set_password(form.cleaned_data['password'])
+            user.save()
+            messages.success(request, 'Utilisateur mis à jour avec succès!')
+            return redirect('user_list')
+    else:
+        form = UserForm(instance=user)
+    return render(request, 'user_manage/usr_edit.html', {'form': form, 'user': user})
+
+def user_delete(request, user_id):
+    user = get_object_or_404(User, id=user_id)
+    if request.method == 'POST':
+        user.delete()
+        messages.success(request, 'Utilisateur supprimé avec succès!')
+        return redirect('user_list')
+    return render(request, 'user_manage/usr_delete.html', {'user': user})
 
 def usr_connect(request):
 	pass
