4530340a7276569fa8ced40c202c748962d4dbdc
qbnt <quentin.banet@hotmail.fr>
2024-07-29 11:19:36 +0200

    refonte de la gestion utilisateur, utilisation des outils de django. Implémentation de la creation de compte, la connexion et la déconnexion.

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -1,11 +1,10 @@
 from django.urls	import path
 from .				import views
 
+app_name = "user_manage"
+
 urlpatterns = [
-    path('', views.user_list, name='user_list'),
-    path('<int:user_id>/', views.user_detail, name='user_detail'),
-    path('add/', views.user_add, name='user_add'),
-    path('edit/<int:user_id>', views.user_edit, name='user_edit'),
-    path('delete/<int:user_id>', views.user_delete, name='user_delete'),
-    # path('usr_connect/', views.usr_connect, name='usr_connect'),
+    path ('login/', views.login_user, name = 'login'),
+	path( 'logout/', views.logout_user, name = 'logout'),
+	path ('register/', views.register_user, name = 'register'),
 ]
--- projet/srcs/web/ft_transcendence/user_manage/views.py
+++ projet/srcs/web/ft_transcendence/user_manage/views.py
@@ -1,54 +1,37 @@
-from django.shortcuts	import render, get_object_or_404, redirect
-from django.contrib 	import messages
-from .models			import User
-from .forms				import UserForm
+from django.contrib.auth.forms	import AuthenticationForm, UserCreationForm, UserChangeForm
+from django.contrib.auth		import authenticate, login, logout
+from django.contrib				import messages
+from django.shortcuts			import render, redirect
 
 
-def user_list(request):
-    users = User.objects.all()
-    context = {'users': users}
-    return render(request, 'user_manage/usr_list.html', context)
+def	login_user(request):
+	if request.method == 'POST':
+		username = request.POST["username"]
+		password = request.POST["password"]
 
-def user_add(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.set_password(form.cleaned_data['password'])
-            user.save()
-            messages.success(request, 'Utilisateur ajouté avec succès!')
-            return redirect('user_list')
-    else:
-        form = UserForm()
-    return render(request, 'user_manage/usr_add.html', {'form': form})
+		user = authenticate(request, username = username, password = password)
 
-def user_detail(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    context = {'user': user}
-    return render(request, 'user_manage/usr_detail.html', context)
+		if user is not None:
+			login(request, user)
+			return redirect("home:index")
+		else:
+			messages.info(request, "Identifiant ou mot de passe incorrect.")
 
-def user_edit(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
-        if form.is_valid():
-            user = form.save(commit=False)
-            if form.cleaned_data['password']:
-                user.set_password(form.cleaned_data['password'])
-            user.save()
-            messages.success(request, 'Utilisateur mis à jour avec succès!')
-            return redirect('user_list')
-    else:
-        form = UserForm(instance=user)
-    return render(request, 'user_manage/usr_edit.html', {'form': form, 'user': user})
+	form = AuthenticationForm()
+	return render(request, "user_manage/login.html", {'form': form})
 
-def user_delete(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    if request.method == 'POST':
-        user.delete()
-        messages.success(request, 'Utilisateur supprimé avec succès!')
-        return redirect('user_list')
-    return render(request, 'user_manage/usr_delete.html', {'user': user})
+def	logout_user(request):
+	logout(request)
+	return redirect("home:index")
 
-def usr_connect(request):
-	pass
+def	register_user(request):
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			return redirect("home:index")
+	else:
+		form = UserCreationForm()
+
+	return render(request, "user_manage/register.html", {'form': form})
