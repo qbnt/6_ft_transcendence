bfc8b5f430019f3b8574307b689ffc58cbef513e
qbnt <quentin.banet@hotmail.fr>
2024-07-29 17:43:23 +0200

    Upgrade css et edition du profil, ajout de l'edition du mot de passe

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -5,7 +5,8 @@ app_name = "user_manage"
 
 urlpatterns = [
     path ('login/', views.login_user, name = 'login'),
-	path( 'logout/', views.logout_user, name = 'logout'),
+	path ( 'logout/', views.logout_user, name = 'logout'),
 	path ('register/', views.register_user, name = 'register'),
-	path ('edit/', views.edit_user, name = 'edit')
+	path ('edit/', views.edit_user, name = 'edit'),
+	path ('edit/pw_update/', views.pw_update, name = 'pw_update'),
 ]
