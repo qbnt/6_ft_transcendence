525d9b22f42de81e3c3fea3e702168d26d9ccb3a
qbnt <quentin.banet@hotmail.fr>
2024-08-03 21:54:00 +0200

    Refonte de la visualisation du profil. Refonte de la page de connexion. Finalisation du système de demande d’amis.
    Il reste à ajouter la possibilité de bloquer des utilisateurs et d’avoir une acceptation des demandes d’amis.

--- projet/srcs/web/ft_transcendence/user_manage/urls.py
+++ projet/srcs/web/ft_transcendence/user_manage/urls.py
@@ -4,16 +4,18 @@ from .				import views
 app_name = "user_manage"
 
 urlpatterns = [
-    path ('login/', views.login_user, name = 'login'),
+    # path ('login/', views.login_user, name = 'login'),
 	path ('logout/', views.logout_user, name = 'logout'),
-	path ('register/', views.register_user, name = 'register'),
+	path ('connexion/', views.login_or_register, name='connexion'),
+	# path ('register/', views.register_user, name = 'register'),
 
-	path ('profile/', views.profile, name='profile'),
+	path ('profile/<str:username>', views.profile, name='profile'),
 	path ('edit/', views.edit_user, name = 'edit'),
 	path ('edit/pw_update/', views.pw_update, name = 'pw_update'),
 
-	path ('add_friend/', views.add_friend, name='add_friend'),
+	path ('add_friend/<str:friend>', views.add_friend, name='add_friend'),
     path ('remove_friend/<str:username>/', views.remove_friend, name='remove_friend'),
+	path ('search/', views.search, name='search'),
 
 	path ('api_42_login/', views.api_42_login, name='api_42_login'),
     path ('api_42_callback/', views.api_42_callback, name='api_42_callback'),
