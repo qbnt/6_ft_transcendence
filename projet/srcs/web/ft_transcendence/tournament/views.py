01c008d9c126dc85f345ba257014c635caf88542
Miguel_BIGPC <miguelescobar704@gmail.com>
2024-08-28 14:32:06 +0200

    Début du tournoi, mise en place des premiers steps

--- projet/srcs/web/ft_transcendence/tournament/views.py
+++ projet/srcs/web/ft_transcendence/tournament/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts   import render, redirect
+from django.http        import HttpResponse
+from .models	        import *
 
-def some_view(request):
+def start(request):
     return HttpResponse("Hello, this is some view!")
+
+def setup_players(request):
+    if request.method == 'POST':
+        # Récupérer tous les noms des joueurs envoyés depuis le formulaire
+        for key, value in request.POST.items():
+            if key.startswith('player'):
+                Players.players.append(value)
+
+        return render(request, 'Confirmation_Tournoi.html', {'players': Players.players})
+
+    return redirect('/')
